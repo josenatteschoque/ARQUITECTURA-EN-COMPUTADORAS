3f18a51d5917af3ffeda0b5a101a3ec0be04422c
josenatteschoque <josenatteschoque@gmail.com>
2026-04-12 23:57:56 -0300

    Ejercicio Finalizado!

--- Tp4/Ejercicio2.asm
+++ Tp4/Ejercicio2.asm
@@ -2,18 +2,61 @@
 #multiplicar del 1 al 10, mostrando cada resultado.
 	
 	.data
-mensaje:	.asciz "Ingrese un numero:\n"
-tabla:	.asciz "Su tabla de multiplicar es:\n"
-
+mensaje:	.asciz "Ingrese un numero:"
+salto:	.asciz "\n"
+multi:	.asciz "X"
+igual:	.asciz "="
 	.text
 main:	
+	#Muestra el mensaje
+	li a7, 4
+	la a0, mensaje
+	ecall
+	
+	#Lee el dato
 	li a7, 5
 	ecall
 	mv t0, a0
 	
+	li t1, 1	#Inicio el contador
+
 loop:	
-	mul  
-	#Falta!!
+	#Muestro el multiplicando
+	mv a0, t0
+	li a7,1
+	ecall
+	
+	#Muestro la X
+	li a7, 4
+	la a0, multi
+	ecall
+
+	#Muestro el multiplicador
+	li a7, 1
+	mv a0, t1
+	ecall
+	
+	#Muestro el igual
+	li a7, 4
+	la a0, igual
+	ecall
+	
+	#Realizo la operacio
+	mul t2, t0, t1	#t0=s1*t1
+	
+	#muestro la operacio
+	mv a0, t2
+	li a7, 1
+	ecall
+	
+	#Muestra el salto de linea
+	li a7, 4
+	la a0, salto
+	ecall
+	
+	addi t1, t1, 1	#Contador++
+	li t5, 11	#t5 = 11
+	blt t1, t5, loop	#Repetir hasta t1 < 11
 	
 	
 	#Finaliza el programa 
